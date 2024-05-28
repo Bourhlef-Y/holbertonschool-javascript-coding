c62aea965dcd0c2967e590aba6ed9ea3bcf63814
JakShyrak <yass.play04@gmail.com>
2024-05-28 09:13:43 +0200

    update

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,14 +1,8 @@
-// Display the initial message
-console.log("Welcome to Holberton School, what is your name?");
-
-// Listen for user input
-process.stdin.on('data', (data) => {
-    const name = data.toString().trim();
-    console.log(`Your name is: ${name}`);
+if (require.main === module) {
+  console.log('Welcome to Holberton School, what is your name?');
+  process.stdin.on('data', (input) => {
+    process.stdout.write(`Your name is: ${input.toString()}`);
+    console.log('This important software is now closing');
     process.exit();
-});
-
-// When the process is about to exit, display the closing message
-process.on('exit', () => {
-    console.log("This important software is now closing");
-});
+  });
+}
